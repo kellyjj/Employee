5fba8a2ab57100b2a338e285e74a83ae228a9783
Kelly J <kellyjj@yahoo.com>
2022-01-08 14:17:23 -0800

    1st committ

---
+++ Models/Employee.cs
@@ -0,0 +1,38 @@
+    using System;  
+    using System.ComponentModel.DataAnnotations;  
+      
+    namespace Employee.Models  
+    {  
+        public class Employee
+        {
+            [Key]
+            public string EMPLOYEEID {get; set;}
+            public string FNAME {get; set;}
+            public string LNAME {get; set;}
+            //public List<EMPROLES> EMPROLE {get; set;}
+            public bool ISMANAGER {get;set;}
+
+            public Employee()
+            {
+                // EMPROLE = new List<EMPROLES>();
+                EMPLOYEEID = string.Empty;
+                FNAME = string.Empty;
+                LNAME = string.Empty;
+                ISMANAGER = false;
+            }
+            
+        }
+
+        public class EMPROLES
+        {
+            [Key]
+            public string CODE {get; set;}
+            public string DESCRIPTION {get; set; }
+
+            public EMPROLES()
+            {
+                CODE = string.Empty;
+                DESCRIPTION = string.Empty;
+            }
+        }
+    }  
